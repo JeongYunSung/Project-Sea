2f59ac67a50b6e831b044f03cb226e46a0082f5e
jeongyunsung <123dbstjd@naver.com>
2020-10-08 04:48:37 +0900

    update

--- member/src/main/java/com/yunseong/member/service/MemberService.java
+++ member/src/main/java/com/yunseong/member/service/MemberService.java
@@ -1,6 +1,8 @@
 package com.yunseong.member.service;
 
+import com.yunseong.member.domain.Member;
 import com.yunseong.member.domain.MemberRepository;
+import io.eventuate.tram.events.publisher.ResultWithEvents;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,4 +13,8 @@ public class MemberService {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    public ResultWithEvents<Member> test() {
+        return null;
+    }
 }
